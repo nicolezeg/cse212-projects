97639cec222944a91058e64f7cb10eca4cc9bff7
nicolezeg <nzegarra07@gmail.com>
2026-09-12 15:38:36 -0500

    dynamic arrays done

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -8,12 +8,24 @@ public static class Arrays
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     public static double[] MultiplesOf(double number, int length)
     {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // First, I need to create an array that will hold all the multiples.
+        // The size of the array should be the same as the length provided.
+        // Next, I will use a for loop to go through each position of the array.
+        // The loop will start at position 0 and continue until it reaches the given length.
+        // For each position, I will calculate the multiple by multiplying the
+        // starting number by the position plus one.
+        // I will store each calculated multiple in the corresponding position
+        // of the array.
+        // After all the multiples have been added to the array, I will return the array. 
 
-        return []; // replace this return statement with your own
+    double[] multiples = new double[length];
+
+    for (int i = 0; i < length; i++)
+    {
+        multiples[i] = number * (i + 1);
+    }
+
+        return multiples; 
     }
 
     /// <summary>
@@ -25,9 +37,19 @@ public static class Arrays
     /// </summary>
     public static void RotateListRight(List<int> data, int amount)
     {
-        // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // First, get the part of the list that will move to the beginning.
+        // This part starts at the position data.Count - amount.
+        // Then, get the part of the list that will remain at the end.
+        // Clear the original list so the elements can be added back in the new order.
+        // Finally, add the two parts back to the original list, starting with the second part.
+
+        List<int> firstPart = data.GetRange(0, data.Count - amount); 
+        List<int> secondPart = data.GetRange(data.Count - amount, amount);
+        
+        data.Clear();
+        
+        data.AddRange(secondPart); 
+        data.AddRange(firstPart);
+
     }
 }
